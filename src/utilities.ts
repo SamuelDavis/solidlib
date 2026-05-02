b8c971bce6c3c40fefac547d46a51b4e211e180f
Samuel Davis <samueljakdavis@gmail.com>
2026-05-02 17:21:20 -0400

    attempt to save data corruption; wrap form onInput

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,5 +1,5 @@
 import { createEffect, type Setter, type Signal } from "solid-js";
-import { isFunction, isKeyed, isString } from "./guards";
+import { assert, isFunction, isInstanceOf, isKeyed, isString } from "./guards";
 import { SetStoreFunction } from "solid-js/store";
 import { AnyRecord, KeyOfUnion } from "./types";
 
@@ -8,6 +8,7 @@ type PersistOpts<T> = {
   key: string;
   encode?: (value: T) => string;
   decode?: (value: string) => T;
+  onError?: (e: Error) => void;
 };
 type Mut<T> = Signal<T> | Store<T>;
 export function persist<T>(signal: Store<T>, opts: PersistOpts<T>): Store<T>;
@@ -16,10 +17,23 @@ export function persist<T>(mut: Mut<T>, opts: PersistOpts<T>): Mut<T> {
   const [get, set] = mut;
   // TODO: Signal and SetStoreFunction have no overlap
   const setter = set as unknown as (cb: () => T) => any;
-  const { key, encode = JSON.stringify, decode = JSON.parse } = opts;
+  const {
+    key,
+    encode = JSON.stringify,
+    decode = JSON.parse,
+    onError = console.error,
+  } = opts;
 
   const item = localStorage.getItem(key);
-  if (isString(item)) setter(() => decode(item));
+  try {
+    if (isString(item)) setter(() => decode(item));
+  } catch (e) {
+    assert(isInstanceOf, e, Error);
+    onError(new Error(e.message, { cause: item }));
+    const value = isFunction(get) ? get() : get;
+    localStorage.setItem(key, encode(value));
+  }
+
   createEffect(() => {
     const value = isFunction(get) ? get() : get;
     return localStorage.setItem(key, encode(value));
@@ -39,15 +53,26 @@ export function onInput<
 ): (event: E) => void {
   return (event: E): void => {
     const value = mut(event.currentTarget.value, event);
-    // @ts-ignore
+    // @ts-expect-error
     // Each member of the union type 'Setter<T> | SetStoreFunction<T>' has signatures,
     // but none of those signatures are compatible with each other. [2349]
     set((prev) => ({ ...prev, [key]: value }));
   };
 }
 
-export function preventDefault(event: Event) {
-  event.preventDefault();
+export function preventDefault<E extends Event>(event: E): void;
+export function preventDefault<E extends Event>(
+  handler: (event: E) => unknown,
+): (event: E) => void;
+export function preventDefault<E extends Event>(
+  input: E | ((e: E) => unknown),
+) {
+  if (isInstanceOf(input, Event)) input.preventDefault();
+  else
+    return (event: E) => {
+      preventDefault(event);
+      return input(event);
+    };
 }
 
 export function extract<T, K extends PropertyKey & KeyOfUnion<T>>(
